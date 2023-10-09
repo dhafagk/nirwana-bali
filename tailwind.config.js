cbd82fe3bda9a5623bc93ec5f8f2c54c1a70f059
dhafagk <dhafaggtq@gmail.com>
2023-10-09 16:37:07 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    backgroundImage: {
+      "main-bg": "url('../img/bg.png')",
+    },
+    fontFamily: {
+      roxborough: "RoxboroughCF, Arial, sans-serif",
+      times: "Times New Roman, Times, serif",
+      gotham: "Gotham, Arial, sans-serif",
+    },
+  },
+  plugins: [],
+};
