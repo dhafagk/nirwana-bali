30959254304167e121ceda0577cefc5f12d647bf
dhafagk <dhafaggtq@gmail.com>
2023-10-10 14:09:37 +0700

    updatE

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,72 @@
+import MyForm from "./Form";
+import ChevronDown from "./assets/chevron-down.svg";
+
 function App() {
   return (
     <>
-      <div className="bg-main-bg bg-no-repeat bg-cover w-full min-h-screen flex flex-col px-2 min-[375px]:px-5 py-[70px] h-full">
-        <div className="flex flex-col gap-10 items-center text-center h-full">
-          <div className="max-w-[100px] mx-auto">
-            <img
-              src="../img/LOGONT2PUTIH.png"
-              className="w-full h-full"
-              alt="Nirwana Textile"
-            />
-          </div>
-          <div className="h-[60vh] flex flex-col gap-10 justify-center">
-            <div>
-              <p className="font-gotham">YOU ARE INVITED TO A</p>
-              <p className="font-roxborough text-3xl min-[375px]:text-4xl font-bold text-[#E5B465] tracking-wider">
-                GRAND OPENING STORE
-              </p>
+      <div className="w-full snap-y h-screen overflow-y-scroll relative">
+        <div className="bg-main-bg bg-no-repeat bg-cover w-full min-h-screen px-2 min-[375px]:px-5 py-[70px] h-full snap-center">
+          <div className="flex flex-col gap-10 items-center text-center h-full">
+            <div className="max-w-[100px] mx-auto">
+              <img
+                src="../img/LOGONT2PUTIH.png"
+                className="w-full h-full"
+                alt="Nirwana Textile"
+              />
             </div>
-            <div>
-              <h2 className="font-times text-[#E5B465] text-xl tracking-wide">
-                NIRWANA FABRIC OUTLET
-              </h2>
-              <div className="py-5">
-                <img
-                  src="../img/divider.png"
-                  alt="divider"
-                  className="translate-y-0.5"
-                />
-                <div className="flex text-[#FAF0B2] justify-evenly items-center font-times tracking-wide">
-                  <span className="min-[375px]:text-lg">SATURDAY</span>
-                  <div className="border-l border-[#D1B15C] h-28 mr-1" />
-                  <div className="flex flex-col font-bold">
-                    <span className="tracking-wider">OCT</span>
-                    <span className="text-6xl tracking-wide leading-[0.8] translate-x-0.5">
-                      21
-                    </span>
-                    <span className="tracking-wider">2023</span>
+            <div className="h-[60vh] flex flex-col gap-10 justify-center">
+              <div>
+                <p className="font-gotham">YOU ARE INVITED TO A</p>
+                <p className="font-roxborough text-3xl min-[375px]:text-4xl font-bold text-[#E5B465] tracking-wider">
+                  GRAND OPENING STORE
+                </p>
+              </div>
+              <div>
+                <h2 className="font-times text-[#E5B465] text-xl tracking-wide">
+                  NIRWANA FABRIC OUTLET
+                </h2>
+                <div className="py-5">
+                  <img
+                    src="../img/divider.png"
+                    alt="divider"
+                    className="translate-y-0.5"
+                  />
+                  <div className="flex text-[#FAF0B2] justify-evenly items-center font-times tracking-wide">
+                    <span className="min-[375px]:text-lg">SATURDAY</span>
+                    <div className="border-l border-[#D1B15C] h-28 mr-1" />
+                    <div className="flex flex-col font-bold">
+                      <span className="tracking-wider">OCT</span>
+                      <span className="text-6xl tracking-wide leading-[0.8] translate-x-0.5">
+                        21
+                      </span>
+                      <span className="tracking-wider">2023</span>
+                    </div>
+                    <div className="border-l border-[#D1B15C] h-28 ml-1" />
+                    <span className="min-[375px]:text-lg">AT 09.00 AM</span>
                   </div>
-                  <div className="border-l border-[#D1B15C] h-28 ml-1" />
-                  <span className="min-[375px]:text-lg">AT 09.00 AM</span>
+                  <img
+                    src="../img/divider.png"
+                    alt="divider"
+                    className="-translate-y-0.5"
+                  />
                 </div>
-                <img
-                  src="../img/divider.png"
-                  alt="divider"
-                  className="-translate-y-0.5"
-                />
-              </div>
-              <div className="flex flex-col text-[#FAF0B2] font-times gap-2">
-                <span className="font-bold">LOCATION :</span>
-                <a
-                  href="https://maps.app.goo.gl/8AihwiD1Px1GfJqLA"
-                  target="_blank"
-                >
-                  JL. IMAM BONJOL NO.332, DENPASAR, BALI
-                </a>
+                <div className="flex flex-col text-[#FAF0B2] font-times gap-2">
+                  <span className="font-bold">LOCATION :</span>
+                  <a
+                    href="https://maps.app.goo.gl/8AihwiD1Px1GfJqLA"
+                    target="_blank"
+                  >
+                    JL. IMAM BONJOL NO.332, DENPASAR, BALI
+                  </a>
+                </div>
               </div>
             </div>
+            <a className="absolute bottom-0 mb-6 animate-bounce" href="#form">
+              <img src={ChevronDown} className="w-10" />
+            </a>
           </div>
         </div>
+        <MyForm />
       </div>
     </>
   );
---
+++ src/Form.tsx
@@ -0,0 +1,96 @@
+import { useForm } from "react-hook-form";
+
+const MyForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const postData = async (url: string, body: any) => {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    return response.json();
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = async (body: any) => {
+    await postData("https://api-gateway.nirwanatextile.com/api/reservasi", body)
+      .then(() => {
+        alert("Data berhasil disimpan");
+        location.reload();
+      })
+      .catch(() => {
+        alert("Data gagal disimpan");
+        location.reload();
+      });
+  };
+
+  return (
+    <div
+      className="h-screen bg-main-bg bg-no-repeat bg-cover w-full snap-center py-10 px-5"
+      id="form"
+    >
+      <span className="font-gotham font-bold text-xl text-white">
+        Konfirmasi Kehadiran
+      </span>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-6 pt-3">
+          <input
+            type="text"
+            placeholder="Nama"
+            {...register("nama", { required: true })}
+            className="bg-gray-100 rounded p-2 text-gray-800 border border-solid border-gray-100 placeholder-slate-500"
+          />
+          <input
+            type="tel"
+            placeholder="No. Hp / WA"
+            {...register("telp", {
+              required: true,
+              minLength: 6,
+              maxLength: 13,
+            })}
+            className="bg-gray-100 rounded p-2 text-gray-800 border border-solid border-gray-100 placeholder-slate-500"
+          />
+          <input
+            type="text"
+            placeholder="Nama Perusahaan"
+            {...register("perusahaan", {
+              required: true,
+            })}
+            className="bg-gray-100 rounded p-2 text-gray-800 border border-solid border-gray-100 placeholder-slate-500"
+          />
+
+          <div>
+            <label className="flex bg-gray-100 text-gray-700 rounded-md px-3 py-2 my-3 hover:bg-[#E5B465] cursor-pointer ">
+              <input
+                type="radio"
+                {...register("hadir", { required: true })}
+                value="ya"
+              />
+              <span className="pl-2">Hadir</span>
+            </label>
+
+            <label className="flex bg-gray-100 text-gray-700 rounded-md px-3 py-2 my-3 hover:bg-[#E5B465] cursor-pointer ">
+              <input
+                type="radio"
+                {...register("hadir", { required: true })}
+                value="tidak"
+              />
+              <span className="pl-2">Tidak Hadir</span>
+            </label>
+          </div>
+
+          <button
+            type="submit"
+            className="bg-[#D1B15C] font-gotham font-bold text-white pt-5"
+          >
+            Kirim
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default MyForm;
