7f30ece350f0d73e0e8433cfda7eb6d6cc5fd8a8
stnlyowen <stnlyowen@gmail.com>
2024-04-17 10:30:23 +0700

    tes bug ip

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <div className="w-full snap-y min-h-screen overflow-y-scroll">
         <div className="bg-main-bg bg-no-repeat bg-cover h-screen w-full px-2 min-[375px]:px-5 snap-start relative">
-          <div className="flex flex-col gap-20 items-center text-center h-50 wrapper pt-10">
+          <div className="flex flex-col gap-20 items-center text-center h-full wrapper pt-10">
             <div className="max-w-[100px] mx-auto">
               <img
                 src="../img/LOGONT2PUTIH.png"
